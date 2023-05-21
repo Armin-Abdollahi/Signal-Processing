6a9b813a4de289314edac9ff4007b3c0fd33f4fd
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-05-21 22:20:32 +0330

    Add files via upload

--- Butterworth_Filter.m
+++ Butterworth_Filter.m
@@ -1,1 +1,6 @@
-clc;
+[A,B,C,D] = butter(10,[500 560]/750);
+d = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',500,'HalfPowerFrequency2',560,'SampleRate',1500);
+
+sos = ss2sos(A,B,C,D);
+fvt = fvtool(sos,d,'Fs',1500);
+legend(fvt,'butter','designfilt')
