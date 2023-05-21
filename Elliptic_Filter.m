63ce5337817b1dd45f73ccdbed8e2c1b62fcaead
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-05-21 22:12:33 +0330

    Add files via upload

--- Elliptic_Filter.m
+++ Elliptic_Filter.m
@@ -1,1 +1,14 @@
-clc;
+n = 5;
+fc = 2e9;
+
+[ze,pe,ke] = ellip(n,3,30,2*pi*fc,"s");
+[be,ae] = zp2tf(ze,pe,ke);
+[he,we] = freqs(be,ae,4096);
+
+plot([we]/(2e9*pi), ...
+    mag2db(abs([he])))
+axis([0 5 -45 5])
+grid
+xlabel("Frequency (GHz)")
+ylabel("Attenuation (dB)")
+legend(["ellip"])
