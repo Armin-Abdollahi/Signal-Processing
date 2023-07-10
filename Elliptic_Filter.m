f08bf3268c2006e7c9a77cb85622f1d4dcaddcb7
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2023-07-10 23:50:40 +0330

    Add files via upload

--- Elliptic_Filter.m
+++ Elliptic_Filter.m
@@ -1,14 +1,37 @@
-n = 5;
-fc = 2e9;
+%% Making a signal
+fs = 1000;                   % sampling frequency (Hz)
+t = 0:1/fs:1-1/fs;           % time axis
+f = 10;                      % signal frequency (Hz)
+x = sin(2*pi*f*t);           % Making signal with frequency f
 
-[ze,pe,ke] = ellip(n,3,30,2*pi*fc,"s");
-[be,ae] = zp2tf(ze,pe,ke);
-[he,we] = freqs(be,ae,4096);
+%% applying noise to the signal
+SNR = 10;                                                % signal to noise ratio (dB)
+noise = randn(size(x));                                  % noise generation with Gaussian distribution
+noise = noise / norm(noise) * norm(x) / (10^(SNR/20));   % Comparison of noise and signal energy based on SNR
+x_noisy = x + noise;                                     % Adding noise to the signal
 
-plot([we]/(2e9*pi), ...
-    mag2db(abs([he])))
-axis([0 5 -45 5])
-grid
-xlabel("Frequency (GHz)")
-ylabel("Attenuation (dB)")
-legend(["ellip"])
+%% Apply elliptic filter
+[n, Wn] = ellipord(40/100, 50/100, 3, 30); % Calculation of degree and filter coefficients
+[b, a] = ellip(n, 3, 30, 40/100);      % Elliptic filter design
+freqz(b,a)
+x_filtered = filter(b, a, x_noisy); % Apply filter on noisy signal
+
+%% Show the main signal, noisy signal and signal after filter application
+figure
+subplot(3,1,1);
+plot(t,x);
+title('main signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(3,1,2);
+plot(t,x_noisy);
+title('Noisy signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(3,1,3);
+plot(x_filtered);
+title('signal after applying the filter');
+xlabel('Time (s)');
+ylabel('Amplitude');
